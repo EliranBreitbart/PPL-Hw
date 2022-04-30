b2c4d509f6a6a54e4ed04d4fa3811895bcc6cbe9
Talyof <yoffe.tal@gmail.com>
2022-04-30 23:05:06 +0300

    general

--- ex2/src/q4.ts
+++ ex2/src/q4.ts
@@ -3,7 +3,8 @@ import { Exp, Program } from '../imp/L3-ast';
 import { isEmptySExp, isSymbolSExp, valueToString, isCompoundSExp } from '../imp/L3-value';
 import { bind } from '../shared/optional';
 import { Result, makeFailure, makeOk, isFailure } from '../shared/result';
-import { IfExp, NumExp, BoolExp, DefineExp, StrExp, PrimOp, VarRef ,VarDecl, AppExp, ProcExp, LetExp, Binding, isAppExp, isBoolExp, isDefineExp, isIfExp, isLetExp, isLitExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarRef, LitExp, CExp   } from './L31-ast';
+import { IfExp, NumExp, BoolExp, DefineExp, StrExp, PrimOp, VarRef ,VarDecl, AppExp, ProcExp, LetExp, Binding, isAppExp, isBoolExp, isDefineExp, isIfExp, isLetExp, isLitExp, isNumExp, isPrimOp, isProcExp, isProgram, isStrExp, isVarRef, LitExp, CExp   } from '../imp/L3-ast'
+;
 /*
 Purpose: Transform L3 AST to JavaScript program string
 Signature: l30ToJS(l2AST)
@@ -41,6 +42,7 @@ const unparsePrimOp = (op : string) : string => {
     op === "eq?" ? "===" :
     op;
 }
+
 export const unparseL3 = (exp: Program | Exp): string =>
     isBoolExp(exp) ? valueToString(exp.val) :
     isNumExp(exp) ? valueToString(exp.val) :
@@ -52,11 +54,10 @@ export const unparseL3 = (exp: Program | Exp): string =>
     isAppExp(exp) ? unparseAppExp(exp.rator, exp.rands) :
     isPrimOp(exp) ? unparsePrimOp(exp.op) :
     isLetExp(exp) ? unparseLetExp(exp) :
-    isDefineExp(exp) ? `const = ${exp.var.var} ${unparseL3(exp.val)})` :
-    isProgram(exp) ? `(L3 ${unparseLExps(exp.exps)})` :
+    isDefineExp(exp) ? `const ${exp.var.var} = ${unparseL3(exp.val)}` :
+    isProgram(exp) ? `${unparseLExps(exp.exps)}` :
     exp;
    
-
 export const l30ToJS = (exp: Exp | Program): Result<string>  => {
     return makeOk(unparseL3(exp));
 }
